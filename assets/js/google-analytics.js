930ac1be403ede71a257abd4699327971f61df6a
Alan Yeung <alansfyeung@gmail.com>
2022-07-10 21:33:40 +1000

    Add tagging

---
+++ assets/js/google-analytics.js
@@ -0,0 +1,12 @@
+(function () {
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-4H628ECGLM';
+    document.head.appendChild(script);
+}());
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-4H628ECGLM');
